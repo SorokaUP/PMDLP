b8c661bd098300ba72bf572b1c391c40a88b42ca
Vladislav <vlad.wath@yandex.ru>
2021-12-07 15:48:15 +0300

    Проведен рефакторинг кода. Компонент ZXing заменен другим компонентом на его основе com.budiyev.android:code-scanner. Добавлена проверка разрешений. Требуется переделать участок проверки разрешений, так как при запрете использования камеры вываливается ошибка.

--- app/src/main/java/com/profitmed/mdlp/model/RestApiSettings.kt
+++ app/src/main/java/com/profitmed/mdlp/model/RestApiSettings.kt
@@ -1,8 +1,11 @@
 package com.profitmed.mdlp.model
 
+import com.profitmed.mdlp.BuildConfig
+import java.util.*
+
 object RestApiSettings {
     object PmSettings {
-        private const val ADDRESS_BASE_API = "http://192.168.127.78:8085/api/"
+        private const val ADDRESS_BASE_API = "http://${BuildConfig.REST_API_HOST}/api/"
         private const val ENDPOINT = "v1"
         const val ADDRESS = "$ADDRESS_BASE_API$ENDPOINT/"
     }
--- app/src/main/java/com/profitmed/mdlp/ui/ScanFragment.kt
+++ app/src/main/java/com/profitmed/mdlp/ui/ScanFragment.kt
@@ -1,16 +1,17 @@
 package com.profitmed.mdlp.ui
 
+import android.Manifest
 import android.content.Context
-import android.media.Image
+import androidx.core.app.ActivityCompat
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.google.zxing.Result
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
@@ -19,22 +20,19 @@ import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.single.PermissionListener
 import com.profitmed.mdlp.R
 import com.profitmed.mdlp.databinding.ScanFragmentBinding
-import com.profitmed.mdlp.model.Repository
 import com.profitmed.mdlp.viewmodel.AppState
 import com.profitmed.mdlp.viewmodel.ScanViewModel
-import me.dm7.barcodescanner.zxing.ZXingScannerView
-import android.view.animation.AlphaAnimation
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.profitmed.mdlp.model.RestApi
-import kotlinx.android.synthetic.main.activity_main.*
 
-class ScanFragment : Fragment(), PermissionListener, ZXingScannerView.ResultHandler {
+class ScanFragment : Fragment(), PermissionListener {
 
-    private lateinit var scannerView: ZXingScannerView
+    private lateinit var codeScanner: CodeScanner
     private lateinit var binding: ScanFragmentBinding
     private val viewModel: ScanViewModel by lazy {
         ViewModelProvider(this).get(ScanViewModel::class.java)
@@ -56,12 +54,23 @@ class ScanFragment : Fragment(), PermissionListener, ZXingScannerView.ResultHand
 
         const val KEY_DID = "DID"
         const val KEY_KIZ = "KIZ"
+
+        const val PERMISSION_REQUEST_CODE  = 1
     }
+
+
+
+    //----------------------------------------------------------------------------------------------
+    // INITIAL
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        if (!checkPermissions()) {
+            requestPermission()
+        }
+
         binding = ScanFragmentBinding.inflate(inflater, container, false)
         setHasOptionsMenu(true)
 
@@ -70,12 +79,51 @@ class ScanFragment : Fragment(), PermissionListener, ZXingScannerView.ResultHand
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
-        init()
 
         if (savedInstanceState != null) {
             kiz = savedInstanceState.getString(KEY_KIZ, "")
-            scanDid(savedInstanceState.getString(KEY_DID, ""))
+            did = savedInstanceState.getString(KEY_DID, "")
         }
+
+        if (checkPermissions()) {
+            go()
+        }
+        else {
+            requestPermission()
+        }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(), arrayOf(
+                Manifest.permission.CAMERA
+            ),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun go() {
+        init()
+        initClickListeners()
+
+        scanDid(did)
+        changeModeDid()
+    }
+
+    private fun init() {
+        val scannerView = binding.scannerView
+        val activity = requireActivity()
+        codeScanner = CodeScanner(activity, scannerView)
+        codeScanner.decodeCallback = DecodeCallback {
+            activity.runOnUiThread {
+                scannerCallback(it.text)
+            }
+        }
+
+        Dexter.withActivity(this.activity)
+            .withPermission(android.Manifest.permission.CAMERA)
+            .withListener(this)
+            .check()
 
         // Сообщаем фрагменту, о модели данных, с которой он будет общаться
         // Сразу же подписываемся на обновления всех данных от этой модели данных
@@ -83,21 +131,103 @@ class ScanFragment : Fragment(), PermissionListener, ZXingScannerView.ResultHand
             // Действие, выполняемое по случаю обновления данных в поставщике
             renderData(it)
         })
+    }
 
+    private fun initClickListeners() {
         binding.inputDidLayout.setEndIconOnClickListener {
             changeModeDid()
         }
-
         binding.fabScan.setOnClickListener {
             startScanner()
         }
-
-        changeModeDid()
     }
 
-    override fun handleResult(rawResult: Result?) {
-        val code = rawResult?.toString() ?: ""
+    private fun renderData(appState: AppState) {
+        binding.resultLayout.visibility = View.GONE
+        when (appState) {
+            is AppState.Success -> {
+                binding.loadingLayout.visibility = View.GONE
+                successAction(appState.res.ID)
+                showCurrentScanMode()
+                viewModel.getLiveData().value = AppState.Idle
+            }
+            is AppState.Loading -> {
+                binding.loadingLayout.visibility = View.VISIBLE
+                stopScanner()
+            }
+            is AppState.Error -> {
+                binding.loadingLayout.visibility = View.GONE
+                errorAction(appState.error.message ?: getString(R.string.rest_api_error))
+                showCurrentScanMode()
+                viewModel.getLiveData().value = AppState.Idle
+            }
+            else -> {
+                // Простой
+            }
+        }
+    }
 
+    //region Разрешения
+    private fun checkPermissions(): Boolean {
+        return checkPermissionCamera()
+    }
+
+    private fun checkPermissionCamera(): Boolean {
+        return ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+        showToast(getString(R.string.permission_grand_camera))
+    }
+
+    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+        showToast(getString(R.string.permission_denied_camera))
+    }
+
+    override fun onPermissionRationaleShouldBeShown(
+        permission: PermissionRequest?,
+        token: PermissionToken?
+    ) {
+        showToast("onPermissionRationaleShouldBeShown")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        var isAllGranted = true
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            // На случай, если будет запрос более одного разрешения
+            for (g in grantResults) {
+                if (g != PackageManager.PERMISSION_GRANTED) {
+                    isAllGranted = false
+                }
+            }
+            if (isAllGranted) {
+                go()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+    //endregion
+
+
+
+    //----------------------------------------------------------------------------------------------
+    // WORK
+
+    //region Работа с камерой
+    private fun startScanner() {
+        codeScanner.startPreview()
+    }
+
+    private fun stopScanner() {
+        if (this::codeScanner.isInitialized)
+            codeScanner.stopPreview()
+    }
+    //endregion
+
+    private fun scannerCallback(code: String) {
         if (isDidMode) {
             scanDid(code)
         }
@@ -134,49 +264,13 @@ class ScanFragment : Fragment(), PermissionListener, ZXingScannerView.ResultHand
         changeModeDid()
     }
 
-    private fun init() {
-        scannerView = binding.zxscan
-        Dexter.withActivity(this.activity)
-            .withPermission(android.Manifest.permission.CAMERA)
-            .withListener(this)
-            .check()
-    }
-
-    private fun renderData(appState: AppState) {
-        binding.resultLayout.visibility = View.GONE
-        when (appState) {
-            is AppState.Success -> {
-                binding.loadingLayout.visibility = View.GONE
-                successAction(appState.res.ID)
-                showCurrentScanMode()
-                viewModel.getLiveData().value = AppState.Idle
-            }
-            is AppState.Loading -> {
-                binding.loadingLayout.visibility = View.VISIBLE
-                stopScanner()
-            }
-            is AppState.Error -> {
-                binding.loadingLayout.visibility = View.GONE
-                errorAction(appState.error.message ?: getString(R.string.rest_api_error))
-                showCurrentScanMode()
-                viewModel.getLiveData().value = AppState.Idle
-            }
-            else -> {
-                // Простой
-            }
-        }
-    }
-
+    //region Реакция системы на ответ от RestApi
     private fun successAction(resId: Int) {
-        context?.let {
-            it.showBottomSheet(getString(R.string.added_id) + resId.toString(), R.drawable.ic_ok_circle)
-        }
+        context?.showBottomSheet(getString(R.string.added_id) + resId.toString(), R.drawable.ic_ok_circle)
     }
 
     private fun errorAction(errMsg: String) {
-        context?.let {
-            it.showBottomSheet(errMsg, R.drawable.ic_cancel_circle)
-        }
+        context?.showBottomSheet(errMsg, R.drawable.ic_cancel_circle)
     }
 
     private fun Context.showBottomSheet(msg: String, iconId: Int) {
@@ -187,35 +281,23 @@ class ScanFragment : Fragment(), PermissionListener, ZXingScannerView.ResultHand
             (this.findViewById(R.id.modalImgRes) as ImageView?)?.setImageResource(iconId)
         }.show()
     }
+    //endregion
 
-    private fun startScanner() {
-        scannerView.setResultHandler(this)
-        scannerView.startCamera()
+
+
+    //----------------------------------------------------------------------------------------------
+    // SYSTEM
+
+    override fun onResume() {
+        super.onResume()
+        if (checkPermissionCamera())
+            codeScanner.startPreview()
     }
 
-    private fun stopScanner() {
-        if (this::scannerView.isInitialized)
-            scannerView.stopCamera()
-    }
-
-    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
-        showToast(getString(R.string.permission_grand_camera))
-    }
-
-    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
-        showToast(getString(R.string.permission_denied_camera))
-    }
-
-    override fun onPermissionRationaleShouldBeShown(
-        permission: PermissionRequest?,
-        token: PermissionToken?
-    ) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onDestroy() {
-        stopScanner()
-        super.onDestroy()
+    override fun onPause() {
+        if (checkPermissionCamera())
+            codeScanner.releaseResources()
+        super.onPause()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
